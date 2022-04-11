af8dcfcc511eb3fe4875d2e5e14a259f5dc2a83d
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-04-11 17:06:53 +0700

    Add files via upload

--- Covid19MV/FE/mainpage.php
+++ Covid19MV/FE/mainpage.php
@@ -3,162 +3,46 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="https://cdn.glitch.me/3e30c75a-6d2a-4b95-8549-1941573d75cf%2F6c47512d-c83d-47d0-9b3a-f8cf28aade64.image.png?v=1638268575758" />
+    <link rel="icon" href="../Pics/SOM.png" />
 
-    <title>Đại Học Quốc Gia</title>
+    <title>VACCINATE AGAINST COVID-19</title>
 
     <!-- import the webpage's stylesheet -->
     <link rel="stylesheet" href="../CSS/mainpage.css" />
   </head>
   <body>
-    <div class="container">
-      <nav>
-        <div class="Home">
-          <a href="#Title">
-            <img
-              src="https://cdn.glitch.me/3e30c75a-6d2a-4b95-8549-1941573d75cf%2Fhome.png?v=1638179317687"
-              alt=""
-            />
-          </a>
-          <div class="user-icon">
-            <a href="ProfileInfor.html">
-              <img
-                src="https://cdn.glitch.me/3e30c75a-6d2a-4b95-8549-1941573d75cf%2Fuser-icon.png?v=1638179317687"
-                alt=""
-              />
-            </a>
-          </div>
-        </div>
-        <div class="Menu">
-          <ul>
-            <li1>
-              <a href="#TBtable">Thông báo</a>
-            </li1>
-            <li2>
-              <a href="registersub.html">Đăng kí học phần</a>
-            </li2>
-
-            <li3>
-              <a href="editcourse.html">Hiệu chỉnh ĐKHP</a>
-            </li3>
-
-            <li4>
-              <a href="tuitionfee.html">Thông tin học phí</a>
-            </li4>
-          </ul>
-        </div>
-        <div class="KhungSearch">
-          <input class="search" type="search" placeholder="Tìm kiếm..." />
-        </div>
-      </nav>
-
+    <div class = "container">
       <header>
-        <div class="title" id="Title">
+      <div class="title" id="Title">
           <ul>
-            ĐẠI HỌC QUỐC GIA THÀNH PHỐ HỒ CHÍ MINH TRƯỜNG ĐẠI HỌC CÔNG NGHỆ
-            THÔNG TIN
+              LOOK UP AND VACCINATE AGAINST COVID-19
           </ul>
         </div>
-        <div class="UIT-logo">
+        <div class="SOM_logo">
           <img
-            src="https://cdn.glitch.me/3e30c75a-6d2a-4b95-8549-1941573d75cf%2FUIT-logo.png?v=1638179317978"
+            src="../Pics/SOM.png"
           />
         </div>
       </header>
 
-      <article>
-        <div class="Panel4">
-          <ul>
-            <a href="#"
-              >> Thông báo thi CK HK1 NH 2021-2022 và ĐKHP các lớp buổi tối, chủ
-              nhật
-            </a>
-            - 25/11/2021 - 11:03
-            <br />
-            <br />
-            <a href="#">> Thông báo Kế hoạch xét Tốt nghiệp đợt 4 năm 2021 </a>
-            - 23/11/2021 - 11:13
-            <br />
-            <br />
-            <a href="#">> Thông báo lịch ĐKHP trong HK2 NH 2021-2022 </a>
-            - 22/11/2021 - 14:23
-            <br />
-            <br />
-            <a href="#"
-              >> Thông báo về việc thu học phí ĐỢT 2, Học kỳ 1, năm học
-              2021-2022 Hệ ĐTĐH Chính quy
-            </a>
-            - 14/09/2021 - 15:48
-            <br />
-            <br />
-            <a href="#"
-              >> Hướng dẫn triển khai dạy và học qua mạng trong thời gian phòng
-              chống dịch COVID-19
-            </a>
-            - 27/03/2020 - 11:28
-          </ul>
-        </div>
-        <div class="Panel3"></div>
-        <div class="Thongbaotable" id="TBtable">
-          <ul>
-            Thông Báo Chung
-          </ul>
-        </div>
+      <nav>
+        <div class = "Menu">
+        <ul>
+            <li1>
+              <a href="#TBtable">Home</a>
+            </li1>
+            <li2>
+              <a href="registersub.php">Injection Registration</a>
+            </li2>
 
-        <div class="Panel6">
-          <ul>
-            <a href="#"
-              >> Thông báo V/v đăng ký lịch học và đóng học phí Anh văn sơ cấp 1
-              & 2 năm học 2021-2022
-            </a>
-            - 14/10/2021 - 08:44
-            <br />
-            <br />
-            <a href="#"
-              >> Thông báo về việc phúc khảo môn học cuối HK 2 NH 2020-2021
-            </a>
-            - 06/09/2021 - 13:41
-            <br />
-            <br />
-            <a href="#"
-              >> Thông báo thời khóa biểu học kỳ 1 năm học, 2021-2022
-            </a>
-            - 06/09/2021 - 11:54
-            <br />
-            <br />
-            <a href="#"
-              >> Khảo sát lấy ý kiến Sinh viên về hoạt động giảng dạy trực tuyến
-              của Giảng viên Học kỳ II - Năm học 2020-2021
-            </a>
-            - 26/07/2021 - 09:25
-            <br />
-            <br />
-            <a href="#"
-              >> Thông báo về việc thay đổi quy trình nộp khóa luận tốt nghiệp
-              sau khi bảo vệ trước hội đồng
-            </a>
-            - 24/02/2021 - 16:19
+            <li3>
+              <a href="editcourse.php">Vaccination lookup</a>
+            </li3>
           </ul>
         </div>
-        <div class="Panel5"></div>
-        <div class="Tinnoibat">
-          <ul>
-            Tin Nổi Bật
-          </ul>
-        </div>
+      </nav>
 
-        <div class="Panel7">
-          <ul>
-            > Chưa có thông báo
-          </ul>
-        </div>
-        <div class="Panel8"></div>
-        <div class="Thongbaocanhan">
-          <ul>
-            Thông Báo Cá Nhân
-          </ul>
-        </div>
-      </article>
-    </div>
+    </div> 
+
   </body>
 </html>
