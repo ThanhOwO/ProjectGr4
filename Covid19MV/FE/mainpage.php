5533c69f3c6234b46b16e37358fa56eb107ef6ab
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-04-14 15:07:57 +0700

    Add files via upload

--- Covid19MV/FE/mainpage.php
+++ Covid19MV/FE/mainpage.php
@@ -4,45 +4,52 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="../Pics/SOM.png" />
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>VACCINATE AGAINST COVID-19</title>
 
     <!-- import the webpage's stylesheet -->
     <link rel="stylesheet" href="../CSS/mainpage.css" />
   </head>
   <body>
-    <div class = "container">
-      <header>
-      <div class="title" id="Title">
-          <ul>
-              LOOK UP AND VACCINATE AGAINST COVID-19
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="Logo">
+           <a  href="#"> <img  src="../Pics/SOM.png"
+              alt="" /> </a>
+          </div>
+          <ul class="Title" >
+        <a href="#">LOOK UP AND VACCINATE AGAINST COVID-19</a>
           </ul>
-        </div>
-        <div class="SOM_logo">
-          <img
-            src="../Pics/SOM.png"
-          />
-        </div>
-      </header>
-
-      <nav>
-        <div class = "Menu">
-        <ul>
-            <li1>
-              <a href="#TBtable">Home</a>
-            </li1>
-            <li2>
-              <a href="registersub.php">Injection Registration</a>
-            </li2>
-
-            <li3>
-              <a href="editcourse.php">Vaccination lookup</a>
-            </li3>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+           <li class="nav-item active">
+             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+             <a class="nav-link" href="#">Injection Registration <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               Search
+             </a>
+             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+               <a class="dropdown-item" href="#">Search for your Covid-19 vaccine certificate</a>
+               <a class="dropdown-item" href="#">Search for your registration reference number</a>
+               <a class="dropdown-item" href="#">Search for feedback</a>
+              </div>
+            </li>
           </ul>
         </div>
       </nav>
+    </header>
 
-    </div> 
 
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
